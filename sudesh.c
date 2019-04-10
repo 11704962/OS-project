9dfd7af1fffe2539a8b022be8e781805b2be318a
11704962 <30723652+11704962@users.noreply.github.com>
2019-04-10 19:46:25 +0530

    Update sudesh.c

--- sudesh.c
+++ sudesh.c
@@ -1,9 +1,16 @@
 
+
 #include<stdio.h>
 #include<windows.h>
 
 int n, fc=0, sc=0, jc=0;
 int quantum;
+
+void display();
+void processDetails();
+void join();
+void roundRobin();
+void report();
 
 struct query
 {
@@ -17,6 +24,160 @@ struct query
 	int rbt;
 
 }fac[100], stu[100], m[100];
+
+
+ main()
+ 
+ {
+ 	
+	display();
+	processDetails();
+	join();
+	roundRobin();
+	report();
+
+}
+
+void display()
+
+{
+	printf("\n\n\tPLEASE FOLLOW THE BELOW INSTRUCTIONS FOR PROPER FUNCTIONING OF THE PROGRAM"
+			"\n\n\n\t>Enter arrival time in 2400 hours format i.e 11:20 will be written as 1120"
+			"\n\n\t>Query arrival times to be entered in real time i.e in ascending order\n"
+			"\n\n\t>Query ID entered should be unique and should be an integer value\n"
+			"\n\n\tAll Time units are in minutes.(Resolving Time and Time quantums are to be entered in minutes) \n\n"
+			"\n\n\t>PRESS ENTER TO CONTINUE.....\n"
+			);
+			getch();
+}
+
+
+void processDetails()
+
+{system("cls");
+	int priority,  i, t;
+	printf("\n\n\tEnter total no of queries: "); scanf("%d", &n);
+	if(n==0) { printf("\n No queries to solve\n"); }
+	else{
+		printf("\n\tEnter time quantum for each Process: "); scanf("%d", &quantum);
+		printf("\n\tEnter 1 for faculty and 2 for student\n");
+		for(i=0; i<n; i++){
+			printf("\n\t%d.Query type (Enter either 1 or 2): ",i+1); scanf("%d", &priority);
+			if(priority==1)
+			{
+				printf("\n\tQuery Id: "); scanf("%d", &fac[fc].qid);
+				fac[fc].priid=1;
+				printf("\n\tArrival Time: "); scanf("%d", &t);
+				if(t<1000 || t>1200){
+
+					printf("\nEnter Correct time\n");
+					processDetails();
+				}
+				else{fac[fc].at= t-1000;}
+				printf("\n\tResolving Time: "); scanf("%d", &fac[fc].bt);	 fac[fc].rbt= fac[fc].bt;
+				if(fac[fc].bt<=0)
+				{
+					printf("\n\n\tResolving time cannot be less than or equal to zero Press enter");
+					getch();
+				processDetails();
+				}
+				
+				fc++;
+			}
+		
+			else if(priority==2)
+			{
+				printf("\n\tQuery Id: "); scanf("%d", &stu[sc].qid);
+				stu[sc].priid=2;
+				printf("\n\tArrival Time: "); scanf("%d", &t);
+				if(t<1000 || t>1200){
+					printf("\n\n\tEnter Correct time\n");
+					processDetails();
+				}
+				else {stu[sc].at= t-1000; }
+				printf("\n\tResolving Time: "); scanf("%d", &stu[sc].bt);	 stu[sc].rbt= stu[sc].bt;
+					if(stu[sc].bt<=0)
+				{
+					printf("\n\n\tResolving time cannot be less than or equal to zero Press enter");
+						getch();
+				processDetails();
+				}
+			
+				sc++;
+			}
+			
+			else
+			{
+			printf("\n\n\tInvalid Entry Press Enter");
+			getch();
+			processDetails();
+			}
+		}
+	}
+}
+
+
+void join()
+
+{
+	int jsc=0, jfc= 0, min, flag;
+	if( fc!=0  && sc!=0){
+		while(jsc<sc && jfc<fc){
+			if(fac[jfc].at == stu[jsc].at){
+				m[jc] = fac[jfc];
+				jc++;
+				jfc++;
+				m[jc]= stu[jsc];
+				jc++;
+				jsc++;
+			}
+			else if(fac[jfc].at < stu[jsc].at){
+				m[jc]= fac[jfc];
+				jc++;
+				jfc++;
+			}
+			else if(fac[jfc].at > stu[jsc].at){
+				m[jc]= stu[jsc];
+				jc++;
+				jsc++;
+			}
+			else;
+		}
+		if(jc != (fc+sc)){
+			if(fc!=jfc){
+				while(jfc!=fc){
+					m[jc]= fac[jfc];
+					jc++;
+					jfc++;
+				}
+			}
+			else if(sc!=jsc){
+				while(jsc!=sc){
+					m[jc]= stu[jsc];
+					jc++;
+					jsc++;
+				}
+			}
+		}
+	}
+	else if(fc==0){
+		while(jsc!=sc){
+			m[jc]= stu[jsc];
+			jc++;
+			jsc++;
+		}
+	}
+	else if(sc==0){
+		while(jfc!=fc){
+			m[jc]= fac[jfc];
+			jc++;
+			jfc++;
+		}
+	}
+	else {
+		printf("\n No valid Jobs available\n");
+	}
+}
 
 
 
@@ -74,91 +235,14 @@ void roundRobin()
 	//from 10am to 12pm only
 	if(time>120)
         {
-            printf("\nTime limit exceeded! Results will still show.\n");
+            printf("\n\n\tTime limit exceeded!\n");
         }
-}
-
-void join()
-
-{
-	int jsc=0, jfc= 0, min, flag;
-	if( fc!=0  && sc!=0)
-	{
-		while(jsc<sc && jfc<fc)
-		{
-			if(fac[jfc].at == stu[jsc].at)
-			{
-				m[jc] = fac[jfc];
-				jc++;
-				jfc++;
-				m[jc]= stu[jsc];
-				jc++;
-				jsc++;
-			}
-			else if(fac[jfc].at < stu[jsc].at)
-			{
-				m[jc]= fac[jfc];
-				jc++;
-				jfc++;
-			}
-			else if(fac[jfc].at > stu[jsc].at)
-			{
-				m[jc]= stu[jsc];
-				jc++;
-				jsc++;
-			}
-			else;
-		}
-		if(jc != (fc+sc))
-		{
-			if(fc!=jfc)
-			{
-				while(jfc!=fc)
-				{
-					m[jc]= fac[jfc];
-					jc++;
-					jfc++;
-				}
-			}
-			else if(sc!=jsc)
-			{
-				while(jsc!=sc)
-				{
-					m[jc]= stu[jsc];
-					jc++;
-					jsc++;
-				}
-			}
-		}
-	}
-	else if(fc==0)
-	{
-		while(jsc!=sc)
-		{
-			m[jc]= stu[jsc];
-			jc++;
-			jsc++;
-		}
-	}
-	else if(sc==0)
-	{
-		while(jfc!=fc)
-		{
-			m[jc]= fac[jfc];
-			jc++;
-			jfc++;
-		}
-	}
-	else 
-	{
-		printf("\n No valid Jobs available\n");
-	}
 }
 
 void report()
 
 {
-	int i=0, total=0, sum=0;
+	int i=0, total=0, sum=0,count=0;
 	double avg;
 	printf("\n\n\tDetails of processes executed\n");
 	printf("\n\tMap\tQUERY ID\tARRIVAL TIME\tRESOLVING TIME\tCOMPLETION TIME\tTURN AROUND TIME\tWAITING TIME");
@@ -167,92 +251,65 @@ void report()
 	
 	
 	{
+		
+		if((m[i].cmpt+1000)<1060)
+		{
+		
 		printf("\n\t%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t\t%d",
 		m[i].priid,m[i].qid, (m[i].at+1000), m[i].bt, (m[i].cmpt+1000), (m[i].cmpt-m[i].at), ((m[i].cmpt-m[i].at)- m[i].bt));
-		total= m[i].cmpt;
+        
+		}
+		
+		else if((m[i].cmpt+1000)>=1060 && (m[i].cmpt+1000)<1160)
+		{
+		printf("\n\t%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t\t%d",
+		m[i].priid,m[i].qid, (m[i].at+1000), m[i].bt, (m[i].cmpt+1000+100-60), (m[i].cmpt-m[i].at), ((m[i].cmpt-m[i].at)- m[i].bt));
+			
+		}
+		
+		
+		else if((m[i].cmpt+1000)>=1160)
+		{
+			printf("\n\t%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t\t%d",
+		m[i].priid,m[i].qid, (m[i].at+1000), m[i].bt, (((m[i].cmpt)/(m[i].cmpt))+1200-1), (1200-(m[i].at+1000)-40), ((m[i].cmpt-m[i].at)- m[i].bt));
+		count++;
+		}
+	
+	
+	
+	
+	
+	
+	
+		if(m[i].cmpt+1000>=1100)
+		{
+		
+		total= (m[i].cmpt+1000-1100+60);
+	    
+		}
+		else
+		{
+			total = m[i].cmpt;
+		}
 		sum+= (m[i].cmpt-m[i].at);
+		
+		if(count>0)
+		{
+			total = 120;
+		}
 	}
 	
 	
 	avg = sum/jc;
 	printf("\n\n\tTotal time taken: %d", total);
+	
 	printf("\n\n\tAverage time: %lf", avg);
+	
+
 	printf("\n\n\tExecution Completed");
 
 
 }
 
-void processDetails()
 
-{system("cls");
-	int priority,  i, t;
-	printf("\n\n\tEnter total no of queries: "); scanf("%d", &n);
-	if(n==0) { printf("\n No queries to solve\n"); }
-	else
-	{
-		printf("\n\tEnter time quantum for each Process: "); scanf("%d", &quantum);
-		printf("\n\tEnter 1 for faculty and 2 for student\n");
-		for(i=0; i<n; i++){
-			printf("\n\t%d.Query type (Enter either 1 or 2): ",i+1); scanf("%d", &priority);
-			if(priority==1)
-			{
-				printf("\n\tQuery Id: "); scanf("%d", &fac[fc].qid);
-				fac[fc].priid=1;
-				printf("\n\tArrival Time: "); scanf("%d", &t);
-				if(t<1000 || t>1200){
 
-					printf("\nEnter Correct time\n");
-					processDetails();
-				}
-				else{fac[fc].at= t-1000;}
-				printf("\n\tResolving Time: "); scanf("%d", &fac[fc].bt);	 fac[fc].rbt= fac[fc].bt;
-				fc++;
-			}
-		
-			else if(priority==2)
-			{
-				printf("\n\tQuery Id: "); scanf("%d", &stu[sc].qid);
-				stu[sc].priid=2;
-				printf("\n\tArrival Time: "); scanf("%d", &t);
-				if(t<1000 || t>1200){
-					printf("\n\n\tEnter Correct time\n");
-					processDetails();
-				}
-				else {stu[sc].at= t-1000; }
-				printf("\n\tResolving Time: "); scanf("%d", &stu[sc].bt);	 stu[sc].rbt= stu[sc].bt;
-				sc++;
-			}
-			
-			else
-			{
-			printf("\n\n\tInvalid Entry");
-			processDetails();
-			}
-		}
-	}
-}
-
-void display()
-
-{
-	printf("\n\n\tPLEASE FOLLOW THE BELOW INSTRUCTIONS FOR PROPER FUNCTIONING OF THE PROGRAM"
-			"\n\n\n\t>Enter arrival time in 2400 hours format i.e 11:20 will be written as 1120"
-			"\n\n\t>Query arrival times to be entered in real time i.e in ascending order\n"
-			"\n\n\t>Query ID entered should be unique\n"
-			"\n\n\tAll Time units are in minutes.(Resolving Time and Time quantums are to be entered in minutes) \n\n"
-			"\n\n\t>PRESS ENTER TO CONTINUE.....\n"
-			);
-			getch();
-}
-
- main()
- 
- {
- 	
-	display();
-	processDetails();
-	join();
-	roundRobin();
-	report();
-
-}
